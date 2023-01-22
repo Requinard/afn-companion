4a9db99926fa7b1a4642dc2035b12be5c5dbfaa5
requinard <david@requinard.nl>
2023-01-22 13:58:47 +0100

    init mui stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
 export function App() {
     const [count, setCount] = useState(0);
 
@@ -11,13 +8,6 @@ export function App() {
             <div>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
                 </a>
             </div>
             <h1>Vite + React</h1>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+    createTheme,
+    CssBaseline,
+    StyledEngineProvider,
+    ThemeProvider,
+} from "@mui/material";
+import { purple } from "@mui/material/colors";
 
-import App from "./App";
-import "./index.css";
+import { App } from "./App";
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: purple[600],
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <StyledEngineProvider>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
+        </StyledEngineProvider>
     </React.StrictMode>
 );
