acc410cbefc4d4df3d911728f28fab07433c903e
requinard <david@requinard.nl>
2023-01-22 20:27:52 +0100

    update things for the map

--- src/components/Home/LocationCard.tsx
+++ src/components/Home/LocationCard.tsx
@@ -5,14 +5,23 @@ import L from "leaflet";
 import "./LocationCard.css";
 import HotelIcon from "~icons/material-symbols/hotel?raw";
 import ParkingIcon from "~icons/material-symbols/local-parking?raw";
+import DrinkIcon from "~icons/ep/cold-drink?raw";
 
 const hotelIcon = L.divIcon({
     html: HotelIcon as unknown as string,
+    iconSize: [42, 42],
+    className: "leaflet-marker-venue",
+});
+
+const drinkIcon = L.divIcon({
+    html: DrinkIcon as unknown as string,
+    iconSize: [42, 42],
     className: "leaflet-marker-venue",
 });
 
 const parkingIcon = L.divIcon({
     html: ParkingIcon as unknown as string,
+    iconSize: [42, 42],
     className: "leaflet-marker-parking",
 });
 
@@ -52,13 +61,65 @@ export const LocationCard = () => {
                 </Marker>
                 <Marker
                     position={[52.006994, 4.360852]}
-                    title={"Venue"}
-                    icon={parkingIcon}
+                    title={"Outsite"}
+                    icon={drinkIcon}
                 >
-                    <Popup>Venue</Popup>
+                    <Popup>
+                        <Stack spacing={2}>
+                            <Typography variant={"h6"}>Outsite</Typography>
+                            <Button
+                                component={Link}
+                                href={"https://outsite.nl/"}
+                                target={"_blank"}
+                                rel={"noopener"}
+                                fullWidth
+                            >
+                                More
+                            </Button>
+                        </Stack>
+                    </Popup>
                 </Marker>
-                <Marker position={[52.0111674, 4.3589877]}>
-                    <Popup>Cocktail Bar</Popup>
+                <Marker
+                    position={[52.01121, 4.35874]}
+                    icon={drinkIcon}
+                    title={"John Rabbit"}
+                >
+                    <Popup>
+                        <Stack spacing={2}>
+                            <Typography variant={"h6"}>John Rabbit</Typography>
+                            <Button
+                                component={Link}
+                                href={"https://johnrabbit.nl/"}
+                                target={"_blank"}
+                                rel={"noopener"}
+                                fullWidth
+                            >
+                                More
+                            </Button>
+                        </Stack>
+                    </Popup>
+                </Marker>
+                <Marker
+                    position={[52.00797, 4.36305]}
+                    icon={parkingIcon}
+                    title={"Zuidpoort Garage"}
+                >
+                    <Popup>
+                        <Stack spacing={2}>
+                            <Typography variant={"h6"}>
+                                Zuidpoort Garage
+                            </Typography>
+                            <Button
+                                component={Link}
+                                href={"https://goo.gl/maps/k9aHk1Riuo6iT27j7"}
+                                target={"_blank"}
+                                rel={"noopener"}
+                                fullWidth
+                            >
+                                Google Maps
+                            </Button>
+                        </Stack>
+                    </Popup>
                 </Marker>
             </MapContainer>
         </Card>
